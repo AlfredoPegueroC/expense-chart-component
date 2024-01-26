1ab422dbab27d4fe86ea54a21923fc50d7dcc9c0
AlfredoPegueroC <alfredocapellan20@gmail.com>
2024-01-26 15:12:27 -0400

    done

--- app.js
+++ app.js
@@ -1,33 +1,36 @@
 const days = document.querySelector("#days");
 
-
-function createCharts(day, amount){
+function createCharts(day, amount, max) {
   let content = document.createElement("div");
-  content.classList.add("chart__wrapper")
-  content.classList.add("flex")
+  content.classList.add("day");
   content.innerHTML = `
-    <div class="day">
-      <div class="bar" style="height: ${amount * 2.5}px"></div>
+      <div class="bar ${maxAmount(max, amount)} " data-before="$${amount}" style="height: ${amount * 2.5}px"></div>
       <div class="mon">
         ${day}
       </div>
-    </div>
-  `
-  days.appendChild(content)
-  
-}
 
-async function getData(){
+  `;
+  days.appendChild(content);
+}
+function maxAmount(amount, elem) {
+   if(Math.max(amount) === elem){
+    return "active"
+   };
+}
+async function getData() {
   await fetch("./data.json")
-  .then(res => res.json())
-  .then(data => {
-    data.forEach(element => {
-      createCharts(element.day, element.amount)  
-    });
-
-  })
-  .catch(err => console.log(err))
+    .then((res) => res.json())
+    .then((data) => {
+      let arr = [];
+      for (let i = 0; i < data.length; i++) {
+        arr.push(data[i].amount);
+      }
+      data.forEach((element) => {
+        createCharts(element.day, element.amount, Math.max(...arr));
+      });
+    })
+    .catch((err) => console.log(err));
 }
 
-getData()
+getData();
 // window.addEventListener("DOMContentLoaded")
